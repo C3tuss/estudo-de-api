198a61ce71f3faa6f8f8f3c2421537c029f1ad69
Alexandre <alexandrevilela.sds@gmail.com>
2024-06-21 12:09:25 -0300

    reorganização e conexão com o bd

--- src/app.js
+++ src/app.js
@@ -1,47 +1,50 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnection";
+import livro from "./models/Livro";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco de dados feita com sucesso");
+});
 
 const app = express();
 app.use(express.json());
 
-function buscaLivro(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-};
-
-const livros = [
-    {
-        id: 1,
-        titulo: "Senhor dos Aneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-}); 
-
-app.get("/livros/:id", (req, res) => {
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
+  });
+  
+  app.get("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
+  })
+  
+  app.post("/livros", (req, res) => {
     livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
+    res.status(201).send("livro cadastrado com sucesso");
+  });
+  
+  app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.status(200).json(livros);
-})
-
-export default app;
+  });
+  
+  app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
+  });
+  
+  export default app;
